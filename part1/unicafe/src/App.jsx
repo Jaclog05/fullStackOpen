4b0322935657dbedd04836d737ade1a341cce1f5
Jaider <jaidercueto.1010@gmail.com>
2025-11-26 15:36:30 -0500

    Unicafe, step 4

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,20 +6,16 @@ const Statistics = ({good, neutral, bad}) => {
   const calculateAverageComments = () => (good - bad) / calculateAllComments()
   const calculateGoodPercentage = () => (good / calculateAllComments()) * 100
 
+  if(calculateAllComments() === 0) return <p>No feedback given</p>
+
   return (
     <>
-      <h1>statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
       <p>all {calculateAllComments()}</p>
-      {calculateAllComments() ?
-        <div>
-          <p>average {calculateAverageComments()}</p>
-          <p>positive {calculateGoodPercentage()}%</p>
-        </div>
-        : ''
-      }
+      <p>average {calculateAverageComments()}</p>
+      <p>positive {calculateGoodPercentage()}%</p>
     </>
   )
 }
@@ -35,6 +31,7 @@ const App = () => {
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
+      <h1>statistics</h1>
       <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
