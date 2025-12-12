6457ea4bcbafcfc479788204500c9618e0f9d8e4
Jaider <jaidercueto.1010@gmail.com>
2025-12-12 14:10:57 -0500

    Data for countries, step 3

--- part2/countries/src/components/CountryView.jsx
+++ part2/countries/src/components/CountryView.jsx
@@ -1,6 +1,18 @@
-function CountryView({ country }) {
-  const languagesArray = Object.values(country.languages);
+import { useState, useEffect } from "react";
+const api_key = import.meta.env.VITE_API_KEY;
 
+function CountryView({ country }) {
+  const [weatherData, setWeatherData] = useState(null);
+
+  useEffect(() => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${country.name.common}&units=metric&appid=${api_key}`
+    )
+      .then((response) => response.json())
+      .then((data) => setWeatherData(data));
+  }, []);
+
+  const languagesArray = Object.values(country.languages);
   return (
     <div>
       <h1>{country.name.common}</h1>
@@ -15,6 +27,13 @@ function CountryView({ country }) {
         ))}
       </ul>
       <img src={country.flags.png} alt={country.flags.alt} />
+      <h3>Weather in {country.name.common}</h3>
+      <p>Temperature {weatherData?.main.temp} Celsius</p>
+      <img
+        src={`https://openweathermap.org/img/wn/${weatherData?.weather[0].icon}.png`}
+        alt="icon-weather-image"
+      />
+      <p>Wind {weatherData?.wind.speed} m/s</p>
     </div>
   );
 }
