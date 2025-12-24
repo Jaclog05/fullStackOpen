eaf0d8a3ed35861097a6a6db34bf78062dd5689f
Jaider <jaidercueto.1010@gmail.com>
2025-12-24 09:13:14 -0500

    Phonebook database step 6

--- part3/index.js
+++ part3/index.js
@@ -18,29 +18,6 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :post")
 )
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
 app.get('/api/persons', (request, response, next) => {
   Person.find({}).then(persons => {
     response.json(persons)
@@ -48,15 +25,17 @@ app.get('/api/persons', (request, response, next) => {
   .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-
-  if(person){
-    response.json(person)
-  }else {
-    response.status(404).end()
-  }
+app.get('/api/persons/:id', (request, response, next) => {
+  const id = request.params.id
+  Person.findById(id)
+    .then(returnedPerson => {
+      if(returnedPerson) {
+        return response.json(returnedPerson)
+      }else {
+        return response.status(404).send({ message: 'Person not found' })
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response, next) => {
@@ -101,11 +80,15 @@ app.delete('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.get('/info', (request, response) => {
-  response.send(`
-    <p>Phone book has info for ${persons.length} people</p>
-    <p>${new Date()}</p>
-  `)
+app.get('/info', (request, response, next) => {
+  Person.find({})
+    .then(persons => {
+      response.send(`
+        <p>Phone book has info for ${persons.length} people</p>
+        <p>${new Date()}</p>
+      `)
+    })
+    .catch(error => next(error))
 })
 
 const errorHandler = (error, request, response, next) => {
